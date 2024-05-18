2f1c0eb0ef33ef4a27b399f566faa8139c9b498c
akshaikrishnan <akshaik143@gmail.com>
2024-05-18 20:04:55 +0530

    fixed route isue

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function POST(req: NextRequest, res: NextResponse) {
-    return Response.json({ name: 'hello' });
+    return NextResponse.json({ name: 'hello' });
 }
