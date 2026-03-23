bb093b9f359b3ac7d2b0977cb37e191e0c5227ac
akshaikrishnan <akshaik143@gmail.com>
2026-03-23 22:14:40 +0530

    fix(layout): update layout to reflect repository relocation message

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,15 @@
 'use client';
 import { LayoutProvider } from '../layout/context/layoutcontext';
 import { PrimeReactProvider } from 'primereact/api';
-import 'primereact/resources/primereact.css';
-import 'primeflex/primeflex.css';
-import 'primeicons/primeicons.css';
-import '../styles/layout/layout.scss';
-import '../styles/demo/Demos.scss';
+// import 'primereact/resources/primereact.css';
+// import 'primeflex/primeflex.css';
+// import 'primeicons/primeicons.css';
+// import '../styles/layout/layout.scss';
+// import '../styles/demo/Demos.scss';
 import RootProvider from '@/providers/providers';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import HelpFloat from '@/demo/components/helpFloat';
 import { Analytics } from '@vercel/analytics/react';
-
 interface RootLayoutProps {
     children: React.ReactNode;
 }
@@ -25,13 +24,39 @@ export default function RootLayout({ children }: RootLayoutProps) {
                     rel="stylesheet"
                 ></link>
             </head>
-            <body>
-                <RootProvider>
+            <body
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    minHeight: '100vh',
+                    backgroundColor: '#285A48',
+                    color: '#B0E4CC',
+                    fontSize: '120px',
+                    fontWeight: '700',
+                    flexDirection: 'column',
+                    textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'
+                }}
+            >
+                NOT FOUND
+                <span
+                    style={{
+                        display: 'block',
+                        fontSize: '24px',
+                        fontWeight: '400',
+                        marginTop: '20px',
+                        color: '#B0E4CC'
+                    }}
+                >
+                    This repository is moved to another organization. Please update your
+                    remote URL.
+                </span>
+                {/* <RootProvider>
                     {children}
                     <HelpFloat />
                     <Analytics />
                     <ReactQueryDevtools initialIsOpen={false} />
-                </RootProvider>
+                </RootProvider> */}
             </body>
         </html>
     );
